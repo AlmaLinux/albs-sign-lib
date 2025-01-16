13a1119fc71e4f65fbfb25cbd7b7fe07cefd7b24
Maxim <mpetuhov@almalinux.org>
2025-01-16 18:36:01 +0200

    Add constants

--- src/albs_sign_lib/constants.py
+++ src/albs_sign_lib/constants.py
@@ -1,5 +1,10 @@
 import enum
 
+COMMUNITY_KEY_SUFFIX = 'ALBS community repo'
+
+# Max file size to allow parallel upload for
+DEFAULT_PARALLEL_FILE_UPLOAD_SIZE = 52428800  # 500 MB
+
 class SignStatusEnum(enum.IntEnum):
     SUCCESS = 1
     READ_ERROR = 2
